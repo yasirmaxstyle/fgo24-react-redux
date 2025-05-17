c3663f366b9d14a2d32d7aa4ede3e4dc056bad3b
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-17 17:46:25 +0700

    fix: change main page to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import Router from './Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </StrictMode>,
 )

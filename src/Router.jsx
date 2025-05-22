bbf99dea50c5dfcd61742206ef2983b6999e4810
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-22 13:09:18 +0700

    feat: add redux provider

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router'
+import { Provider } from 'react-redux'
 import App from './App'
 import Result from './pages/Result'
 
+import { store } from './redux/store'
+
 function Router() {
   return (
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/result' element={<Result />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </Provider>
   )
 }
 

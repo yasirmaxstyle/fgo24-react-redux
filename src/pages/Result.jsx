7e442bd23d09f9bf6ce05d52a02e9108e66ac648
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-05 07:46:46 +0700

    fix: switch use of local storage to redux as state storage

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router"
 
+import { useDispatch, useSelector } from "react-redux"
+import { removeData } from "../redux/reducers/survey";
+
+import { MdDelete } from "react-icons/md";
+
 function Result() {
-  const dataSmokers = JSON.parse(window.localStorage.getItem('smokers')) || []
+  // const dataSmokers = JSON.parse(window.localStorage.getItem('smokers')) || []
+  const dispatch = useDispatch()
+  const surveyData = useSelector(state => state.surveyData.data)
+
+  const deleteData = (e) => {
+    // console.log(e.currentTarget.id)""
+    dispatch(removeData(e.currentTarget.id))
+  }
+
   return (
     <div className="max-w-[640px] mx-auto flex flex-col gap-4">
       <div className="bg-white p-3 rounded-lg mt-10 shadow-md border-orange-line border-t-10 py-6">
         <h1 className='text-5xl font-bold text-center'>Hasil Survey Perokok</h1>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md">
-        <table className='table-auto border border-collapse text-center'>
+        <table className='table-auto border *:border **:border border-collapse text-center'>
           <thead>
             <tr>
               <th className='p-2'>No.</th>
@@ -20,7 +33,7 @@ function Result() {
             </tr>
           </thead>
           <tbody>
-            {dataSmokers.map((el, idx) =>
+            {surveyData.map((el, idx) =>
               <tr key={idx}>
                 <td className='p-2'>{idx + 1}</td>
                 <td className='p-2'>{el.name}</td>
@@ -28,6 +41,11 @@ function Result() {
                 <td className='p-2'>{el.gender}</td>
                 <td className='p-2'>{el.isSmoker}</td>
                 <td className='p-2'>{el.brand.join(', ')}</td>
+                <td>
+                  <button id={idx} className="border-none hover:cursor-pointer" onClick={deleteData}>
+                    <MdDelete className="border-none text-3xl" />
+                  </button>
+                </td>
               </tr>
             )}
           </tbody>
